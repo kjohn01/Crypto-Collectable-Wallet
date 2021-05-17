326a813d041b87f15b74f43eba76567307acd4e7
KJohn <mayacafe123@gmail.com>
2021-05-17 18:06:17 +0800

    fetch more list items when scrolled to the bottom

--- src/containers/ListPage.jsx
+++ src/containers/ListPage.jsx
@@ -1,20 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import { Observable, fromEvent } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 import fetchListItems from '../functions';
 
 export default function ListPage() {
   const [listItem, setListItem] = useState([]);
   const [itemCount, setItemCount] = useState(0);
 
+  const fetchData = useCallback(
+    () => {
+      console.log("fetchData");
+      fetchListItems(itemCount).then((data) => {
+        const newItems = data.assets;
+        setListItem([...listItem, ...newItems]);
+        setItemCount(itemCount + newItems.length);
+      });
+    },
+    [itemCount, listItem],
+  )
+
   useEffect(() => {
-    fetchListItems(itemCount).then((data) => {
-      const newItems = data.assets;
-      setListItem([...listItem, ...newItems]);
-      setItemCount(itemCount + newItems.length);
-    });
+    // fetch on page load
+    fetchData();
+    // Bind the fetchData method to the window scrolling event
+    let scrollEvent = fromEvent(window, 'scroll');
+    scrollEvent.pipe(
+      map((e) => ({
+        scrollHeight: e.target.scrollHeight,
+        scrollTop: e.target.scrollTop,
+        clientHeight: e.target.clientHeight
+      })),
+      filter(position => position.scrollTop + position.clientHeight >= position.scrollHeight)
+    ).subscribe(() => fetchData());
     return () => {
 
     }
-  }, [])
+  }, [fetchData]);
 
   return (
     <div>
